13c9b2bf600d59431762889140e66d3d4085b4f3
ababiltalha <1805077@ugrad.cse.buet.ac.bd>
2023-08-11 00:36:42 +0600

    offline 2 part 2 started

---
+++ Offline 2/Part 2/Triangle.cpp
@@ -0,0 +1,19 @@
+#ifndef TRIANGLE_CPP
+#define TRIANGLE_CPP
+
+#include "Point.cpp"
+
+class Triangle {
+public:
+    Point v1, v2, v3;
+
+    Triangle(Point v1, Point v2, Point v3) : v1(v1), v2(v2), v3(v3) {}
+
+    
+};
+
+
+
+
+
+#endif
